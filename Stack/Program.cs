9d95333d455e12ae47c0a5a891bbb6324d170da6
AndreeaTM <andreeateodora.marinica@yahoo.com>
2019-08-27 12:29:29 +0300

    first try

---
+++ Stack/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<string> numbers = new Stack<string>();
+            numbers.Push("one");
+            numbers.Push("two");
+            numbers.Push("three");
+            numbers.Push("four");
+            numbers.Push("five");
+            
+
+            Console.WriteLine($"The last element from the stack is : {numbers.Last()}");
+            Console.WriteLine("The elements from the stack remain unaltered:");
+            foreach (string number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+
+    }
+    
+}
